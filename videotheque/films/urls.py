e53855ef867fc973e88c97887329cbcda95a86c6
BS-JeremyB <jeremy.bazin@bstorm.be>
2024-04-23 16:20:37 +0200

    CRUD film

--- videotheque/films/urls.py
+++ videotheque/films/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('realisateur/', views.realisateur_list, name='liste_realisateur'),
-    path('realisateur/<int:id>', views.realisateur_detail, name='detail_realisateur')
+    path('realisateur/<int:id>', views.realisateur_detail, name='detail_realisateur'),
+    path('film/', views.film_list, name='liste_realisateur'),
+    path('film/<int:id>', views.film_detail, name='detail_realisateur')
 ]
--- videotheque/films/views.py
+++ videotheque/films/views.py
@@ -21,7 +21,7 @@ def realisateur_list(request):
             serializer.save()
             return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
         
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return JsonResponse(status=status.HTTP_400_BAD_REQUEST)
     
 @csrf_exempt
 def realisateur_detail(request, id):
@@ -46,4 +46,46 @@ def realisateur_detail(request, id):
 
     elif request.method == 'DELETE':
         realisateur.delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+    
+
+
+@csrf_exempt
+def film_list(request):
+
+    if request.method == 'GET':
+        films = Film.objects.all()
+        serializer = FilmSerializer(films, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = FilmSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@csrf_exempt
+def film_detail(request, id):
+    try:
+        film = Film.objects.get(id=id)
+    except Film.DoesNotExist:
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = FilmSerializer(film)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = FilmSerializer(instance=film, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+
+    elif request.method == 'DELETE':
+        film.delete()
         return HttpResponse(status=status.HTTP_204_NO_CONTENT)
