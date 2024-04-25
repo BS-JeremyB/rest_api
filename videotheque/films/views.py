2c6a4981f109fa91aa33fcf87dab7a0cc46aa617
BS-JeremyB <jeremy.bazin@bstorm.be>
2024-04-25 15:55:33 +0200

    pagination, filtre, tri

--- videotheque/films/views.py
+++ videotheque/films/views.py
@@ -1,5 +1,7 @@
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 from .models import *
 from .serializers import *
 
@@ -8,6 +10,10 @@ class RealisateurViewSet(viewsets.ModelViewSet):
 
     queryset = Realisateur.objects.all()
     serializer_class = RealisateurSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ['nom', 'nationalite']
+    ordering_fields = ['id', 'nom']
+    ordering = ['-id']
 
     # def get_permissions(self):
     #     if self.action == 'destroy':
--- videotheque/videotheque/settings.py
+++ videotheque/videotheque/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'django_filters',
     'films',
 ]
 
@@ -131,6 +132,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS':['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
     'DEFAULT_AUTHENTICATION_CLASSES':(
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
