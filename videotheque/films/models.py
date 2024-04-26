4237f13d1311309f5d3edd31c5fd14206aa92921
BS-JeremyB <jeremy.bazin@bstorm.be>
2024-04-26 12:02:13 +0200

    upload custom dir

--- videotheque/films/models.py
+++ videotheque/films/models.py
@@ -1,4 +1,18 @@
 from django.db import models
+import os
+from django.utils.text import slugify
+
+#Custom MKDIR
+
+def film_directory_path(instance, filename):
+    # Obtient un titre de film nettoyé et crée un chemin pour l'affiche
+    # Utilise 'slugify' pour assurer la compatibilité du chemin de fichier
+    base_filename, file_extension = os.path.splitext(filename)
+    clean_title = slugify(instance.titre)
+    return f'affiches/{clean_title}/{base_filename}{file_extension}'
+
+
+
 
 # Create your models here.
 class Realisateur(models.Model):
@@ -15,7 +29,7 @@ class Film(models.Model):
     annee_sortie = models.IntegerField()
     realisateur = models.ManyToManyField(Realisateur)
     num_exploitation = models.IntegerField(unique=True)
-    affiche = models.ImageField(upload_to='affiches/', null=True, blank=True)
+    affiche = models.ImageField(upload_to=film_directory_path, null=True, blank=True)
 
     def __str__(self):
         return self.titre
