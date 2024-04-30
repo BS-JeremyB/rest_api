bb417adfede8a5d2e6713db3a0cb92f51adea8e5
BS-JeremyB <jeremy.bazin@bstorm.be>
2024-04-30 11:57:26 +0200

    Reverse M2M

--- videotheque/films/serializers.py
+++ videotheque/films/serializers.py
@@ -4,22 +4,10 @@ from .models import *
 class RealisateurSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Realisateur
-        fields = '__all__'
+        fields = ['url', 'id', 'nom','prenom','nationalite','film_set']
 
 class FilmSerializer(serializers.HyperlinkedModelSerializer):
-    realisateur_id = serializers.PrimaryKeyRelatedField(
-        many=True,
-        queryset=Realisateur.objects.all(),
-        write_only=True,
-        source='realisateur'
-    )
-    realisateur = serializers.HyperlinkedRelatedField(
-        many=True,
-        read_only=True,
-        view_name='realisateur-detail'
-    )
-
     class Meta:
         model = Film
-        fields = ['url','id', 'titre', 'annee_sortie', 'realisateur', 'realisateur_id', 'num_exploitation','affiche']
+        fields = '__all__'
 
--- videotheque/videotheque/urls.py
+++ videotheque/videotheque/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('api/films/', include('films.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path('api/token/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
+    path("api-auth/", include("rest_framework.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
